ec99063f6c7688ed832ed36cfe2360cde7fe7d6c
9Patit <patit.sudsat@gmail.com>
2023-12-16 02:22:23 +0700

    add full update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let data = document.getElementById("number-text");
 let price = document.getElementById("pts");
 let cart = document.getElementById("cart");
+let pp = document.getElementById("pp");
 
 function increase() {
   let currentValue = parseInt(data.innerText, 10);
@@ -18,6 +19,7 @@ function deleteb() {
 
 function updatePrice(currentValue) {
   price.innerText = currentValue * 125; 
+  pp.innerText = currentValue * 250;
 }
 
 function updateCart(currentValue) {
